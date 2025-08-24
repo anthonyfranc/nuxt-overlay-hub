9c5f1c8da1d39762a3007d6dba973d6549f39f74
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2025-08-24 08:42:39 -0400

    Add UserLogin component for user authentication

---
+++ docs/app/components/UserLogin.vue
@@ -0,0 +1,21 @@
+<!-- app/components/auth/UserLogin.vue -->
+<template>
+  <UModal title="Login">
+    <div class="space-y-3">
+      <UInput placeholder="Email" />
+      <UInput placeholder="Password" type="password" />
+
+      <div class="flex justify-end gap-2">
+        <!-- Clicking these buttons will close the overlay -->
+        <UButton @click="$emit('close')">Close</UButton>
+        <UButton color="primary" @click="$emit('close', { ok: true })">
+          Sign in
+        </UButton>
+      </div>
+    </div>
+  </UModal>
+</template>
+
+<script setup lang="ts">
+defineEmits<{ (e: 'close', value?: any): void }>()
+</script>
